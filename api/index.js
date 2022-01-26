da474bcb67f489423da4ab682af9693c54229e39
jpromanonet <programming@jpromano.net>
2022-01-25 23:32:09 -0300

    Connection to mongodb and catching connection errors

--- api/index.js
+++ api/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Setting up the ENV file
+dotenv.config();
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://adminTestNucbaFS:<password>@cluster0.vbqcs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
+.then(() => console.log("DB Connection OK"))
+.catch((err) => console.log(err));
 
 app.listen(3001, ()=> {
     console.log("Backend server is running!!!");
