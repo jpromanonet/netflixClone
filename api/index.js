8845f4bccce1afe274e360148452d4b715417af2
jpromanonet <programming@jpromano.net>
2022-01-25 23:26:35 -0300

    Mongoose

--- api/index.js
+++ api/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://adminTestNucbaFS:<password>@cluster0.vbqcs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
 app.listen(3001, ()=> {
     console.log("Backend server is running!!!");
 })
