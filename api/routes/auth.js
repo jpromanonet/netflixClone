35b4c2871003ae3419a513fc71da82aa658ef26e
jpromanonet <programming@jpromano.net>
2022-01-27 23:42:11 -0300

    Register router

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,4 +2,21 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 // Register
-router.post("");
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.SECRET_KEY
+        ).toString(),
+    });
+    try {
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Login
