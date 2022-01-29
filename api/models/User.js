37cadc6660d0bbbb1d0b90bbe41e0fbe15b25843
jpromanonet <programming@jpromano.net>
2022-01-28 23:58:40 -0300

    User route

--- api/models/User.js
+++ api/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
         username:{type:String, required:true, unique:true},
         email:{type:String, required:true, unique:true},
         password:{type:String, required:true},
-        profilePic:{type:String, required:true},
+        profilePic:{type:String, required:false},
         isAdmin: {type: Boolean, default: false}
     },
     { timestamps: true }
