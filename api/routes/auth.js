9da8c89e762533f5f4326d7152d5c2b513216a3b
jpromanonet <programming@jpromano.net>
2022-01-29 00:17:01 -0300

    User route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -7,7 +7,10 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(
+            req.body.password, 
+            process.env.SECRET_KEY
+        ).toString(),
     });
     try {
         const user = await newUser.save();
