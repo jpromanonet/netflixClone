e1716cf91fa05f59e33ea8157c11a71721779d1c
jpromanonet <programming@jpromano.net>
2022-01-29 00:09:46 -0300

    User route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 // Register
 router.post("/register", async (req, res) => {
