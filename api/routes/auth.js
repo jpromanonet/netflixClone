cb33c015098416777b12c86048cad8e4e3fb5d32
jpromanonet <programming@jpromano.net>
2022-02-01 00:03:47 -0300

    Auth.js router

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -34,7 +34,7 @@ router.post("/login", async (req, res) => {
         originalPassword !== req.body.password &&
             res.status(401).json("Wrong password or username!");
 
-            const accesstoken = jwt.sign({id: user._id, isAdmin: user.isAdmin});
+            const accesstoken = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.SECRET_KEY, { expiresIn: "5d"});
         
         const { password, ...info } = user._doc;
 
