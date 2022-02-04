1e73dd5c7976380e764771bb33edaabb51b7ba57
jpromanonet <programming@jpromano.net>
2022-02-03 23:28:06 -0300

    User router

--- api/verifyToken.js
+++ api/verifyToken.js
@@ -5,7 +5,9 @@ function verify(req, res, next) {
     if(authHeader){
         const token = authHeader.split(" ")[1];
 
-        jwt.verify(token, process.env.SECRET_KEY);
+        jwt.verify(token, process.env.SECRET_KEY,(err, user) => {
+            if(err) res.status(403)
+        });
     } else {
         return res.status(401).json("You are not authenticated!");
     }
