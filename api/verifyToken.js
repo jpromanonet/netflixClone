feba156246284d781d670c7673d6eaf9bb19e7f8
jpromanonet <programming@jpromano.net>
2022-02-03 23:21:09 -0300

    User router

--- api/verifyToken.js
+++ api/verifyToken.js
@@ -2,7 +2,11 @@ const jwt = require("jsonwebtoken")
 
 function verify(req, res, next) {
     const authHeader = req.headers.token;
-    if(authHeader){}
+    if(authHeader){
+        const token = authHeader.split(" ")[1];
+    } else {
+        return res.status(401).json("You are not authenticated!");
+    }
 }
 
 module.exports = verify;
