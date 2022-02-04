1a5e8b15e3ee26e72713c08b0f3ada49bb352fd2
jpromanonet <programming@jpromano.net>
2022-02-03 22:55:52 -0300

    User router

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,1 +1,18 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+
+// Update users
+
+router.put("/:id", async (req, res) => {
+    
+})
+
+// Delete users
+
+// Get
+
+// Get all users
+
+// Get user stats
