55d58aa6dd06bded7d6aa7eb9816618eb5f72d97
jpromanonet <programming@jpromano.net>
2022-02-03 23:34:35 -0300

    User router

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,22 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const verify = require("../verifyToken");
 
 
 // Update users
 
 router.put("/:id", async (req, res) => {
+    if(req.user.id === req.params.id || req.user.isAdmin) {
+        if(req.body.password){
+            req.body.password = CryptoJS.AES.encrypt(
+                req.body.password,
+                process.env.SECRET_KEY
+            ).toString();
+        }
 
+        try{}
+    }
 })
 
 // Delete users
